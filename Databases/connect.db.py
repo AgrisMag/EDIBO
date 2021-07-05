7f23089c23e7a4d1b32af8074d3adad443946e48
Agris <agris.magdalenoks@gmail.com>
2021-07-05 16:07:32 +0300

    Delete connect.db.py

--- Databases/connect.db.py
+++
@@ -1,8 +0,0 @@
-import mysql.connector
-
-mydb = mysql.connector.connect(host='127.0.0.1',
-                               user='root', password='root',
-                               port='3306',
-                               database='music_shop')
-
-mydb.close()
